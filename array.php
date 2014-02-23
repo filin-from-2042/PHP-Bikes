911785cb2f5ec4b1038f76dba4533464b4507531
filin-from-2042 <filin2042@gmail.com>
2014-02-23 17:29:41 +0400

    array modify

--- array.php
+++ array.php
@@ -1,31 +1,4 @@
 <?php
-$bmw=array(
-		"model"=>"X5",
-		"speed"=> 120,
-		"doors"=>5,
-		"year"=>"2006",
-				);
-print_r($bmw);
-
-$toyota=array(
-		"model"=>"Carina",
-		"speed"=> 130,
-		"doors"=>4,
-		"year"=>"2007<	<2<1",
-
-);
-
-
-$toyota=array(
-		"model"=>"X5",
-		"speed"=> 120,
-		"doors"=>5,
-		"year"=>"2006",
-
-);
-
-
-
 
 /*
 ЗАДАНИЕ 1
@@ -38,7 +11,34 @@ $toyota=array(
 - Создайте массивы $toyota и $opel аналогичные массиву $bmw.
 - Заполните массив $toyota значениями: "Carina", 130, 4, "2007"
 - Заполните массив $opel значениями: "Corsa", 140, 5, "2007"		
+?>*/
+
+$bmw=array(
+		"model"=>"X5",
+		"speed"=> 120,
+		"doors"=>5,
+		"year"=>"2006",
+				);
+
+
+$toyota=array(
+		"model"=>"Carina",
+		"speed"=> 130,
+		"doors"=>4,
+		"year"=>"2007<	<2<1",
+
+);
+print_r($toyota);
+
+$toyota=array(
+		"model"=>"X5",
+		"speed"=> 120,
+		"doors"=>5,
+		"year"=>"2006",
+
+);
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
@@ -51,9 +51,10 @@ $toyota=array(
 	<?php
 	/*
 	ЗАДАНИЕ 2
-	- Выведите значения всех трёх массивов в виде: name - model - speed - doors -year,  например: bmw - 120 - 5 - 2006
-	*/
+	- Выведите значения всех трёх массивов в виде: name - model - speed - doors -year,  например: bmw - 120 - 5 - 2006*/
+	
 	?>
 </body>
 </html>
+
 
